d54f012dd5c5c94385fb3338c9b3c1cfe0904062
Polina <46245789+Polina-nest@users.noreply.github.com>
2024-10-29 09:19:39 +0300

    Create SecondFragment.kt

---
+++ SecondFragment.kt
@@ -0,0 +1,31 @@
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.MainViewModel
+import com.example.myapplication.R
+
+class SecondFragment : Fragment() {
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+
+        view.findViewById<View>(R.id.button_second).setOnClickListener {
+            findNavController().navigate(R.id.action_secondFragment_to_thirdFragment)
+            viewModel.setCurrentFragment(R.id.thirdFragment)
+        }
+    }
+}
