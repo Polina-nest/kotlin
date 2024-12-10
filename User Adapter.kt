9e5e96194ecf85bc59bc65822c72514bfa588ea5
Polina <46245789+Polina-nest@users.noreply.github.com>
2024-12-10 09:47:08 +0300

    Create User Adapter.kt

---
+++ User Adapter.kt
@@ -0,0 +1,25 @@
+import android.view.LayoutInflater
+   import android.view.View
+   import android.view.ViewGroup
+   import android.widget.TextView
+   import androidx.recyclerview.widget.RecyclerView
+
+   class UserAdapter(private val userList: List<UserEntity>) : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+       override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+           val view = LayoutInflater.from(parent.context).inflate(R.layout.item_user, parent, false)
+           return UserViewHolder(view)
+       }
+
+       override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+           val user = userList[position]
+           holder.nameTextView.text = user.name
+           holder.emailTextView.text = user.email
+       }
+
+       override fun getItemCount(): Int = userList.size
+
+       class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+           val nameTextView: TextView = itemView.findViewById(R.id.text_view_name)
+           val emailTextView: TextView = itemView.findViewById(R.id.text_view_email)
+       }
+   }
