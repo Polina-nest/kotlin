302a6369d6ee25336dbe011505c542cf6c37b1a4
Polina <46245789+Polina-nest@users.noreply.github.com>
2024-12-10 09:30:57 +0300

    Create User Dao.kt

---
+++ User Dao.kt
@@ -0,0 +1,12 @@
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+    @Insert
+    suspend fun insert(user: UserEntity)
+
+    @Query("SELECT * FROM users")
+    suspend fun getAllUsers(): List<UserEntity>
+}
