e7f54c2919ffed6cc141520a63ed9715f4e49e2e
Polina <46245789+Polina-nest@users.noreply.github.com>
2024-12-10 09:31:37 +0300

    Create AppDatabase.kt

---
+++ AppDatabase.kt
@@ -0,0 +1,24 @@
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import android.content.Context
+
+@Database(entities = [User Entity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+
+    companion object {
+        @Volatile
+        private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return instance ?: synchronized(this) {
+                instance ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).fallbackToDestructiveMigration().build().also { instance = it }
+            }
+        }
+    }
+}
